230207deff8250cf249e11000ec339a4abebdbf1
nikeshbhudia <nikeshbhudia1@gmail.com>
2022-07-07 12:16:06 +0100

    basics1

--- src/PracticeCommands/Basics.java
+++ src/PracticeCommands/Basics.java
@@ -3,8 +3,19 @@ package PracticeCommands;
 public class Basics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String name = "Nikesh";
+		String colour = "Blue";
+		
+		String greeting = ("My name is " + name + ", I like the colour " + colour);
+		System.out.println(greeting);
+		
+		
+		String fullname = "Nikesh Bhudia";
+		
+		String surname = fullname.substring(6);
+		
+		System.out.println(surname);
+		
 	}
-
 }
